62655bb2674b625f59c18c02fb2872b0cdf59315
Kletskov Gleb <gfgfddgleb@gmail.com>
2023-12-09 20:44:25 +0000

    WIP: upload report to s3

--- packages/server/main.go
+++ packages/server/main.go
@@ -1,34 +1,41 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
-	"os"
+	"net/url"
 
 	"github.com/kletskovg/accounting/packages/logger"
-	"github.com/kletskovg/accounting/server/handlers"
+	"github.com/kletskovg/packages/common"
 )
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", handlers.Ping)
-	mux.HandleFunc("/ping", handlers.Ping)
-	mux.HandleFunc("/list", handlers.ListHander)
-	mux.HandleFunc("/add", handlers.AddHandler)
-	mux.HandleFunc("/remove", handlers.RemoveHandler)
-	mux.HandleFunc("/update", handlers.UpdateHandler)
-	mux.HandleFunc("/report", handlers.ReportHandler)
+	// mux := http.NewServeMux()
+	// mux.HandleFunc("/", handlers.Ping)
+	// mux.HandleFunc("/ping", handlers.Ping)
+	// mux.HandleFunc("/list", handlers.ListHander)
+	// mux.HandleFunc("/add", handlers.AddHandler)
+	// mux.HandleFunc("/remove", handlers.RemoveHandler)
+	// mux.HandleFunc("/update", handlers.UpdateHandler)
+	// mux.HandleFunc("/report", handlers.ReportHandler)
 
-	var port = ":8080"
+	// var port = ":8080"
 
-	logger.Info("Server starting on", port)
-	go http.Get("https://telegram.kletskovg.tech/done/accounting_server_started")
+	// logger.Info("Server starting on", port)
+	// go http.Get(fmt.Sprint("%s/%s", common.TelegramApiUrl, "/done/Budget_server_started"))
 
-	logger.Info("IS DEV")
-	os.Getenv("DEV")
+	// logger.Info("IS DEV")
+	// os.Getenv("DEV")
 
-	error := http.ListenAndServe(port, mux)
+	// error := http.ListenAndServe(port, mux)
 
-	if error != nil {
-		logger.Error("Error starting server", error)
-	}
+	// if error != nil {
+	// 	logger.Error("Error starting server", error)
+	// }
+
+	message := "Cant create tmp file with report"
+	logger.Info(message)
+	go http.Get(
+		fmt.Sprint("%s/done/%s", common.TelegramApiUrl, url.QueryEscape(message)),
+	)
 }

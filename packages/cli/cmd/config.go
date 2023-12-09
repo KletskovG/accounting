ca6a665dfa3e6155b653a6092191a6bfefd6762e
Kletskov Gleb <gfgfddgleb@gmail.com>
2023-12-09 14:18:48 +0000

    add CLI binary for MacOS x86

--- packages/cli/cmd/config.go
+++ packages/cli/cmd/config.go
@@ -5,11 +5,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-func configArgsValidator(cmd *cobra.Command, args []string) error {
-	// if !len(args) % 2
-	return nil
-}
-
 func configCommand(cmd *cobra.Command, args []string) {
 	logger.Info("Config args length")
 	logger.Info(len(args) % 2)

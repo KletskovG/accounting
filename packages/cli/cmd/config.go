9e4e84baf26858032053e937acbbc9240d13400d
Kletskov Gleb <gfgfddgleb@gmail.com>
2023-12-09 17:20:37 +0000

    kinda complete with config

--- packages/cli/cmd/config.go
+++ packages/cli/cmd/config.go
@@ -1,11 +1,16 @@
 package cmd
 
 import (
+	"os"
+
 	"github.com/kletskovg/accounting/packages/logger"
+	"github.com/kletskovg/packages/common"
 	"github.com/spf13/cobra"
 )
 
 func configCommand(cmd *cobra.Command, args []string) {
-	logger.Info("Config args length")
-	logger.Info(len(args) % 2)
+	if err := os.Remove(common.CliUserConfigPath); err != nil {
+		logger.Info("Cant remove config file %s", err)
+	}
+	logger.Info("Config file was cleared, run any command to reconfigure")
 }

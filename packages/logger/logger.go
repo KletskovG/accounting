1eb2a779d50fd7511a94cad032aaf2814bd11f89
Kletskov Gleb <gfgfddgleb@gmail.com>
2023-12-11 00:24:25 +0000

    chore: tech debt after launch

--- packages/logger/logger.go
+++ packages/logger/logger.go
@@ -9,11 +9,13 @@ import (
 const errorPrefix = "[ERROR]: "
 const infoPrefix = "[INFO]: "
 
+// Error logs stack and arguments to the console and exit program with Fatalln
 func Error(messages ...any) {
 	stack := debug.Stack()
 	log.Fatalln(string(stack), errorPrefix, messages)
 }
 
+// Info logs prints arguments to the console with prefix
 func Info(messages ...any) {
-	log.Println(infoPrefix, fmt.Sprint(messages))
+	log.Println(infoPrefix, fmt.Sprint(messages...))
 }
